efa1282705171d947d0450d31ade0d9f9549f125
Ukheyon Park <dkdkdkdk555@naver.com>
2025-04-08 15:08:53 +0900

    수정중

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/navigation_bar.dart';
+import 'navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const Center(child: Text('홈')),
+    const Center(child: Text('검색이얌!!')),
+    const Center(child: Text('설정')),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: FloatingNavBarPage(),
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        extendBody: true,
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: IslandNavigationBar(
+          selectedIndex: _selectedIndex,
+          onTap: _onTap,
+        ),
+      ),
     );
   }
 }
-
--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -1,59 +1,42 @@
 import 'package:flutter/material.dart';
 
-class FloatingNavBarPage extends StatefulWidget {
-  const FloatingNavBarPage({super.key});
+class IslandNavigationBar extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onTap;
 
-  @override
-  // ignore: library_private_types_in_public_api
-  _FloatingNavBarPageState createState() => _FloatingNavBarPageState();
-}
-
-class _FloatingNavBarPageState extends State<FloatingNavBarPage> {
-  int _selectedIndex = 0;
-
-  final List<Widget> _pages = [
-    const Center(child: Text('홈')),
-    const Center(child: Text('검색')),
-    const Center(child: Text('설정')),
-  ];
-
-  void _onTap(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+  const IslandNavigationBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true, // 바디 아래까지 확장해서 그림자 등 자연스럽게
-      body: _pages[_selectedIndex],
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: BottomNavigationBar(
-            currentIndex: _selectedIndex,
-            onTap: _onTap,
-            backgroundColor: Colors.white,
-            selectedItemColor: Colors.blue,
-            unselectedItemColor: Colors.grey,
-            elevation: 10,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: '홈',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: '검색',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                label: '설정',
-              ),
-            ],
-          ),
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: BottomNavigationBar(
+          currentIndex: selectedIndex,
+          onTap: onTap,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          elevation: 10,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '홈',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: '검색',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: '설정',
+            ),
+          ],
         ),
       ),
     );
