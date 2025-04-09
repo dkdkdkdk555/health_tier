3de79496eebca9cebd1e3e933a98804e5b07d368
Ukheyon Park <dkdkdkdk555@naver.com>
2025-04-09 17:59:33 +0900

    폴더구조수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/tab/doc/doc_body_calendar.dart';
 import 'navigation_bar.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [
-    const Center(child: Text('기록')),
+    DocBodyCalendar(),
     const Center(child: Text('통계')),
     const Center(child: Text('커뮤니티')),
     const Center(child: Text('회원')),
---
+++ lib/view/tab/doc/doc_body_calendar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DocBodyCalendar extends StatefulWidget {
+  const DocBodyCalendar({super.key});
+
+  @override
+  State<DocBodyCalendar> createState() => _DocBodyCalendarState();
+}
+
+class _DocBodyCalendarState extends State<DocBodyCalendar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
