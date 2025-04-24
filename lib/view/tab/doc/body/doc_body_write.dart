66b495de0f65819082ae8c69c29a7ba429903c84
Ukheyon Park <dkdkdkdk555@naver.com>
2025-04-24 17:58:55 +0900

    1-1-3. 체중기록 입력 페이지 시안구현

--- lib/view/tab/doc/body/doc_body_write.dart
+++ lib/view/tab/doc/body/doc_body_write.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:my_app/extension/screen_ratio_extension.dart';
 
 class DocBodyWrite extends StatefulWidget {
@@ -12,7 +13,14 @@ class _DocBodyWriteState extends State<DocBodyWrite> {
   @override
   Widget build(BuildContext context) {
     final htio = ScreenRatio(context).heightRatio;
-    final wtio = ScreenRatio(context).widthRatio;
+    final wtio = ScreenRatio(context).widthRatio;    
+
+    final TextEditingController _weightEditor = TextEditingController();
+    final TextEditingController _muscleEditor = TextEditingController();
+    final TextEditingController _bodyFatEditor = TextEditingController();
+    final TextEditingController _memoEditor = TextEditingController();
+
+    final stampCollect = ['perfect', 'good', 'normal', 'bad', 'terrible'];
   
     return Container(
       decoration: BoxDecoration(
@@ -69,57 +77,26 @@ class _DocBodyWriteState extends State<DocBodyWrite> {
                                   '2025.03.06 (목)',
                                   style: TextStyle(
                                     color: Color(0xFF777777),
-                                    fontSize: 16.5,
+                                    fontSize: 13.7,
                                     fontFamily: 'Pretendard',
                                     fontWeight: FontWeight.w500
                                   ),
                                 ),
                             ),
                           ),
-                          Container(
-                            height: 1,
-                            decoration: const BoxDecoration(color: Color(0xFFEEEEEE)),
-                          ),
+                          makeBorder(),
                           const Spacer(flex: 12),
-                          const Expanded(
-                            flex: 24,
-                            child: Row(
-                              
-                            ),
-                          ),
+                          inputArea('체중', 'kg', _weightEditor),
                           const Spacer(flex: 12),
-                           const Expanded(
-                            flex: 24,
-                            child: Row(
-                              
-                            ),
-                          ),
+                          inputArea('골격근', 'kg', _muscleEditor),
                           const Spacer(flex: 12),
-                           const Expanded(
-                            flex: 24,
-                            child: Row(
-                              
-                            ),
-                          ),
+                          inputArea('체지방률', '%', _bodyFatEditor),
                           const Spacer(flex: 12),
-                           const Expanded(
-                            flex: 48,
-                            child: Row(
-                              
-                            ),
-                          ),
+                          textArea(_memoEditor),
                           const Spacer(flex: 12),
-                           const Expanded(
-                            flex: 17,
-                            child: Row(
-                              
-                            ),
-                          ),
+                          buttonArea(),
                           const Spacer(flex: 16),
-                          Container(
-                            height: 1,
-                            decoration: const BoxDecoration(color: Color(0xFFEEEEEE)),
-                          ),
+                          makeBorder(),
                           const Spacer(flex: 16),
                           const Expanded( // Text
                             flex: 9,
@@ -131,7 +108,7 @@ class _DocBodyWriteState extends State<DocBodyWrite> {
                                   textAlign: TextAlign.center,
                                   style: TextStyle(
                                       color: Color(0xFF777777),
-                                      fontSize: 14.7,
+                                      fontSize: 12,
                                       fontFamily: 'Pretendard',
                                   ),
                                 )
@@ -139,19 +116,38 @@ class _DocBodyWriteState extends State<DocBodyWrite> {
                             ),
                           ),
                           const Spacer(flex: 8),
-                          const Expanded(
-                            flex: 31,
-                            child: Row(
-                              
-                            ),
-                          ),
+                          setStampCollection(stampCollect),
                           const Spacer(flex: 20),
                           Expanded(
                             flex: 27,
-                            child: Container(
-                              color: Colors.lightBlue,
-                              child: const Row(
-                                
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: FractionallySizedBox(
+                                widthFactor: 1, // 부모(Row)의 width만큼 가로로 꽉 채움
+                                child: GestureDetector(
+                                  onTap: () {
+                                    // 버튼 클릭 로직
+                                  },
+                                  child: Container(
+                                    height: double.infinity, // 세로는 flex: 27 높이 채우기
+                                    decoration: ShapeDecoration(
+                                      color: const Color(0xFF0D85E7),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                    ),
+                                    child: const Center(
+                                      child: Text(
+                                        '확인',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontFamily: 'Pretendard',
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
@@ -166,6 +162,229 @@ class _DocBodyWriteState extends State<DocBodyWrite> {
             ),
           )
         ],
+      ),
+    );
+  }
+
+  Expanded setStampCollection(List<String> stampCollect) {
+    return Expanded(
+      flex: 31,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: stampCollect.map((stampName) {
+          return SizedBox(
+            width: 56.94, // 아이콘 크기 제한
+            height: 56.94,
+            child: SvgPicture.asset(
+              'assets/icons/stamp_$stampName.svg',
+              fit: BoxFit.contain,
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Container makeBorder() {
+    return Container(
+      height: 1,
+      decoration: const BoxDecoration(color: Color(0xFFEEEEEE)),
+    );
+  }
+
+  Expanded buttonArea() {
+    return Expanded(
+      flex: 17,
+      child: Row(
+        children: [
+          const Expanded(
+            flex: 78,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '운동/음주',
+                style: TextStyle(
+                    color: Color(0xFF333333),
+                    fontSize: 14.5,
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w500,
+                    height: 0.09,
+                ),
+              )
+            )
+          ),
+          Expanded(
+            flex: 257,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Row(
+                children: [
+                  makeYnButton('운동 여부', 'assets/icons/work_out.svg'),
+                  const SizedBox(width: 17),
+                  makeYnButton('음주 여부', 'assets/icons/drink.svg'),
+                ],
+              ),
+            )
+          )
+        ],
+      ),
+    );
+  }
+
+  GestureDetector makeYnButton(String text, String iconPath) {
+    return GestureDetector(
+      onTap: () {
+      },
+      child: Container(
+          width: 89,
+          height: 34,
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+          side: BorderSide(width: 1, color: Color(0xFF333333)),
+          borderRadius: BorderRadius.circular(99),
+          ),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 4),
+              child: Text(
+                  text,
+                  style: const TextStyle(
+                    color: Color(0xFF333333),
+                    fontSize: 11,
+                    fontFamily: 'Pretendard',
+                    height: 0.12,
+                ),
+              ),
+            ),
+            Expanded(
+              child: SvgPicture.asset(
+                iconPath,
+                colorFilter: const ColorFilter.mode(
+                Color(0xFF333333),
+                  BlendMode.srcIn,
+                ),
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+
+  Expanded textArea(TextEditingController editor) {
+    return Expanded(
+      flex: 48,
+      child: Row(
+        children: [
+          const Expanded(
+            flex: 78,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                '메모',
+                style: TextStyle(
+                  color: Color(0xFF333333),
+                  fontSize: 14.5,
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w500,
+                  height: 3
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 257,
+            child: TextField(
+              controller: editor,
+              maxLines: null, // 여러 줄
+              keyboardType: TextInputType.multiline,
+              textInputAction: TextInputAction.newline,
+              expands: true, // 남은 공간 전체 사용 -> 이거 해야 height flex:48 다 차지함
+              decoration: InputDecoration(
+                hintText: '메모를 입력해주세요. (최대 100자)\n',
+                hintStyle: const TextStyle(
+                  color: Color(0xFF999999),
+                  fontSize: 13.5,
+                  fontFamily: 'Pretendard',
+                  height: 4,
+                ),
+                contentPadding: const EdgeInsets.all(12), // 여백 추가
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Color(0xFFDDDDDD),
+                    width: 1,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Color(0xFF0D86E7),
+                    width: 1.5,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget inputArea(String text, String unit, TextEditingController _editor) {
+    return Expanded(
+      flex: 24,
+      child: Row(
+        children:[
+          Expanded(
+            flex: 78,
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                  text,
+                  style: const TextStyle(
+                      color: Color(0xFF333333),
+                      fontSize: 14.5,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w500,
+                      height: 0.09,
+                  ),
+              )
+            )
+          ),
+          Expanded(
+            flex: 257,
+            child: TextField(
+              controller: _editor,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                suffixText: unit,
+                suffixStyle: const TextStyle(
+                  color: Color(0xFF999999),
+                  fontSize: 16,
+                  fontFamily: 'Pretendard',
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Color(0xFFDDDDDD),
+                    width: 1,
+                  )
+                ),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  borderSide: const BorderSide(
+                    color: Color(0xFF0D86E7), // 클릭 시 파란색 테두리
+                    width: 1.5,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ]
       ),
     );
   }
