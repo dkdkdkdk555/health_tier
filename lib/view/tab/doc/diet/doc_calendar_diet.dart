8789c6775df5205b4fb8791e8f98d6486e2b6438
dkdkdkdk555 <dkdkdkdk555@naver.com>
2025-05-01 19:21:53 +0900

    .
    - 아이콘 추가
    - 총 섭취량 디자인 구현

--- lib/view/tab/doc/diet/doc_calendar_diet.dart
+++ lib/view/tab/doc/diet/doc_calendar_diet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class DocCalendarDiet extends StatefulWidget {
   const DocCalendarDiet({super.key});
@@ -16,6 +17,99 @@ class _DocCalendarDietState extends State<DocCalendarDiet> {
         decoration: const BoxDecoration(
           color: Color(0xFFFFFFFF),
         ),
+        child: Column(
+          children: [
+            const Expanded(
+              flex: 16,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 4),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '2025년 5월',
+                    style: TextStyle(
+                      color: Color(0xFF333333),
+                      fontSize: 16,
+                      fontFamily: 'Pretendard',
+                      fontWeight: FontWeight.w700,
+                      height: 1.50,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Expanded(
+              flex: 33,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20)
+              ),
+            ),
+            Container(
+                width: 335,
+                height: 1,
+                decoration: const BoxDecoration(color: Color(0xFFEEEEEE)),
+            ),
+            Expanded(
+              flex: 36,
+              child: Padding(padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    makeTotal('assets/icons/kcal.svg', '총 섭취 칼로리', '11,650 kcal'),
+                    const Spacer(flex: 5,),
+                    makeTotal('assets/icons/protein.svg', '총 섭취 단백질', '120g'),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+
+    );
+  }
+
+  Flexible makeTotal(String path, String text, String numunit) {
+    return Flexible(
+      flex: 6,
+      child: Row(
+        children: [
+          SizedBox(
+            width: 20,
+            height: 20,
+            child: SvgPicture.asset(
+              path,
+            )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: Text(
+              text,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontFamily: 'Pretendard',
+                fontWeight: FontWeight.w500,
+                height: 1.50,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                numunit,
+                textAlign: TextAlign.right,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontFamily: 'Pretendard',
+                  fontWeight: FontWeight.w500,
+                  height: 1.50,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
