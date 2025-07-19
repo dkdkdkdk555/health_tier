775e5ecdf6c9fb4074cd075c427e822d710e0125
dkdkdkdk555 <dkdkdkdk555@naver.com>
2025-07-19 13:32:55 +0900

    네이버 로그인 테스트 위젯

---
+++ lib/view/tab/usr/get_started_screen.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/view/tab/usr/agreement/agreement_bottom_bar.dart';
+import 'package:my_app/view/tab/usr/sns_connect/naver_login.dart';
+
+class GetStartedScreen extends StatefulWidget {
+  const GetStartedScreen({super.key});
+
+  @override
+  State<GetStartedScreen> createState() => _GetStartedScreenState();
+}
+
+class _GetStartedScreenState extends State<GetStartedScreen> {
+
+  void _showAgreementBottomBar(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (_) => const AgreementBottomBar(),
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    // Container(
+      // decoration: const BoxDecoration(
+      //   color: Color(0xFFFFFFFF),
+      // ),
+      // child: Column(
+      //   children: [
+          // Padding(
+          //   padding: const EdgeInsets.only(top: 145.0, bottom: 0, left: 20, right: 20),
+          //   child: Icon(
+          //     Icons.accessibility_new,
+          //     size: 148,
+          //     color: Colors.amber.shade800,
+          //   ),
+          // ),
+          // Padding(
+          //   padding: const EdgeInsets.only(bottom: 140),
+          //   child: Text(
+          //     '성장을 눈으로 보는 방법,\n헬스티어',
+          //     textAlign: TextAlign.center,
+          //     style: TextStyle(
+          //       fontFamily: 'Pretendard',
+          //       fontSize: 20,
+          //       color: Colors.amber.shade800
+          //     ),
+          //   ),
+          // ),
+          // const Padding(
+          //   padding: EdgeInsets.all(13.0),
+          //   child: Text(
+          //     '- SNS 간편 로그인 -',
+          //     style: TextStyle(
+          //       fontWeight: FontWeight.w300,
+          //       fontFamily: 'Pretendard',
+          //     ),
+          //   ),
+          // ),
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: [
+          //     GestureDetector(
+          //       onTap: () => _showAgreementBottomBar(context),
+          //       child: Container(
+          //         width: 50, // 버튼의 지름
+          //         height: 50, // 버튼의 지름
+          //         decoration: const BoxDecoration(
+          //           shape: BoxShape.circle, // 원형 모양
+          //           color: Colors.green, // 초록색
+          //         ),
+          //         child: const Align(
+          //           alignment: Alignment.center,
+          //           child: Text(
+          //             'N',
+          //             style: TextStyle(
+          //               fontSize: 21,
+          //               fontWeight: FontWeight.bold,
+          //               color: Color(0xFFFFFFFF),
+          //             ),
+          //           ),
+          //         ),
+          //       ),
+          //     ),
+          //     const SizedBox(width: 15), // 버튼 사이 간격
+
+          //     GestureDetector(
+          //       onTap: () => _showAgreementBottomBar(context),
+          //       child: Container(
+          //         width: 50,
+          //         height: 50,
+          //         decoration: const BoxDecoration(
+          //           shape: BoxShape.circle,
+          //           color: Colors.yellow, // 노란색
+          //         ),
+          //         child: const Align(
+          //           alignment: Alignment.center,
+          //           child: Text(
+          //             'Kakao',
+          //             style: TextStyle(
+          //               fontSize: 14,
+          //               fontWeight: FontWeight.w400,
+          //             ),
+          //           ),
+          //         ),
+          //       ),
+          //     ),
+          //     const SizedBox(width: 15), // 버튼 사이 간격
+
+          //     GestureDetector(
+          //       onTap: () => _showAgreementBottomBar(context),
+          //       child: Container(
+          //         width: 50,
+          //         height: 50,
+          //         decoration: const BoxDecoration(
+          //           shape: BoxShape.circle,
+          //           color: Colors.black, // 검은색
+          //         ),
+          //          child: const Align(
+          //           alignment: Alignment.center,
+          //           child: Icon(
+          //             Icons.apple_outlined,
+          //             color: Color(0xFFFFFFFF),
+          //             size: 26,
+          //           )
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          // ),
+          const NaverLogin();
+        // ],
+      // ),
+    // );
+  }
+
+    
+
+}
+
--- lib/view/tab/usr/sns_connect/naver_login.dart
+++ lib/view/tab/usr/sns_connect/naver_login.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_naver_login/flutter_naver_login.dart';
-import 'package:flutter_naver_login/interface/types/naver_account_result.dart';
 import 'package:flutter_naver_login/interface/types/naver_login_result.dart';
 import 'package:flutter_naver_login/interface/types/naver_login_status.dart';
 import 'package:flutter_naver_login/interface/types/naver_token.dart';
-import 'package:my_app/view/tab/usr/not_user/agreement_bottom_bar.dart';
+import 'package:flutter_naver_login/flutter_naver_login.dart';
+import 'package:flutter_naver_login/interface/types/naver_account_result.dart';
 
-class GetStartedScreen extends StatefulWidget {
-  const GetStartedScreen({super.key});
+
+
+class NaverLogin extends StatefulWidget {
+  const NaverLogin({super.key});
 
   @override
-  State<GetStartedScreen> createState() => _GetStartedScreenState();
+  State<NaverLogin> createState() => _NaverLoginState();
 }
 
-class _GetStartedScreenState extends State<GetStartedScreen> {
+class _NaverLoginState extends State<NaverLogin> {
+
   bool isLogin = false;
   String? accessToken;
   String? expiresAt;
@@ -22,133 +24,8 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
   String? refreshToken;
   NaverAccountResult? userInfo;
 
-  void _showAgreementBottomBar(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      backgroundColor: Colors.transparent,
-      builder: (_) => const AgreementBottomBar(),
-    );
-  }
-
-
   @override
   Widget build(BuildContext context) {
-    return 
-    // Container(
-      // decoration: const BoxDecoration(
-      //   color: Color(0xFFFFFFFF),
-      // ),
-      // child: Column(
-      //   children: [
-          // Padding(
-          //   padding: const EdgeInsets.only(top: 145.0, bottom: 0, left: 20, right: 20),
-          //   child: Icon(
-          //     Icons.accessibility_new,
-          //     size: 148,
-          //     color: Colors.amber.shade800,
-          //   ),
-          // ),
-          // Padding(
-          //   padding: const EdgeInsets.only(bottom: 140),
-          //   child: Text(
-          //     '성장을 눈으로 보는 방법,\n헬스티어',
-          //     textAlign: TextAlign.center,
-          //     style: TextStyle(
-          //       fontFamily: 'Pretendard',
-          //       fontSize: 20,
-          //       color: Colors.amber.shade800
-          //     ),
-          //   ),
-          // ),
-          // const Padding(
-          //   padding: EdgeInsets.all(13.0),
-          //   child: Text(
-          //     '- SNS 간편 로그인 -',
-          //     style: TextStyle(
-          //       fontWeight: FontWeight.w300,
-          //       fontFamily: 'Pretendard',
-          //     ),
-          //   ),
-          // ),
-          // Row(
-          //   mainAxisAlignment: MainAxisAlignment.center,
-          //   children: [
-          //     GestureDetector(
-          //       onTap: () => _showAgreementBottomBar(context),
-          //       child: Container(
-          //         width: 50, // 버튼의 지름
-          //         height: 50, // 버튼의 지름
-          //         decoration: const BoxDecoration(
-          //           shape: BoxShape.circle, // 원형 모양
-          //           color: Colors.green, // 초록색
-          //         ),
-          //         child: const Align(
-          //           alignment: Alignment.center,
-          //           child: Text(
-          //             'N',
-          //             style: TextStyle(
-          //               fontSize: 21,
-          //               fontWeight: FontWeight.bold,
-          //               color: Color(0xFFFFFFFF),
-          //             ),
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //     const SizedBox(width: 15), // 버튼 사이 간격
-
-          //     GestureDetector(
-          //       onTap: () => _showAgreementBottomBar(context),
-          //       child: Container(
-          //         width: 50,
-          //         height: 50,
-          //         decoration: const BoxDecoration(
-          //           shape: BoxShape.circle,
-          //           color: Colors.yellow, // 노란색
-          //         ),
-          //         child: const Align(
-          //           alignment: Alignment.center,
-          //           child: Text(
-          //             'Kakao',
-          //             style: TextStyle(
-          //               fontSize: 14,
-          //               fontWeight: FontWeight.w400,
-          //             ),
-          //           ),
-          //         ),
-          //       ),
-          //     ),
-          //     const SizedBox(width: 15), // 버튼 사이 간격
-
-          //     GestureDetector(
-          //       onTap: () => _showAgreementBottomBar(context),
-          //       child: Container(
-          //         width: 50,
-          //         height: 50,
-          //         decoration: const BoxDecoration(
-          //           shape: BoxShape.circle,
-          //           color: Colors.black, // 검은색
-          //         ),
-          //          child: const Align(
-          //           alignment: Alignment.center,
-          //           child: Icon(
-          //             Icons.apple_outlined,
-          //             color: Color(0xFFFFFFFF),
-          //             size: 26,
-          //           )
-          //         ),
-          //       ),
-          //     ),
-          //   ],
-          // ),
-          getNaverLogin();
-        // ],
-      // ),
-    // );
-  }
-
-  getNaverLogin() {
     return SingleChildScrollView(
       child: SizedBox(
         height: 1000,
@@ -228,7 +105,7 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
     );
   }
 
-  Future<void> buttonLoginPressed() async {
+   Future<void> buttonLoginPressed() async {
     try {
       final NaverLoginResult res = await FlutterNaverLogin.logIn();
       setState(() {
@@ -304,4 +181,3 @@ class _GetStartedScreenState extends State<GetStartedScreen> {
     }
   }
 }
-
