b25a488cbfa6ea92ccb0fc2bbdacd62fd117f6c0
dkdkdkdk555 <dkdkdkdk555@naver.com>
2025-08-26 01:19:47 +0900

    .

--- lib/view/tab/usr/usr_main/usr_info_tab_bar.dart
+++ lib/view/tab/usr/usr_main/usr_info_tab_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class UsrInfoTabBar extends StatefulWidget {
-   final int selectedIndex;
+  final int selectedIndex;
   final ValueChanged<int> onTap;
 
   const UsrInfoTabBar({
@@ -15,8 +15,7 @@ class UsrInfoTabBar extends StatefulWidget {
 }
 
 class _UsrInfoTabBarState extends State<UsrInfoTabBar> {
-  final List<String> tabs = ['신체 정보', '뱃지', '내가 쓴 글'];
-  final List<GlobalKey> _tabKeys = [];
+  final List<String> tabs = ['신체 정보', '뱃지', '내가 쓴 글']; // 첫 번째 코드 스타일에 맞춰 탭 순서 변경
   
   late int selectedIndex;
 
@@ -25,65 +24,80 @@ class _UsrInfoTabBarState extends State<UsrInfoTabBar> {
 
   bool _isFirstBuild = true;
 
+  // 탭의 고정 너비와 간격을 정의합니다.
+  final double fixedTabWidth = 80; // 탭의 너비를 동일하게 맞추기 위한 임의의 값
+  final double tabSpacing = 20;
+
   @override
   void initState() {
     super.initState();
     selectedIndex = widget.selectedIndex;
-    // tabs 맴버별 GlobalKey를 생성
-    _tabKeys.addAll(List.generate(tabs.length, (_) => GlobalKey()));
-
+    
+    // GlobalKey는 더 이상 텍스트 크기를 측정하는 용도가 아니므로 제거
+    // 대신, WidgetsBinding.instance.addPostFrameCallback 내에서 바로 위치를 계산합니다.
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _updateUnderline();
       _isFirstBuild = false;
     });
+  }
+  
+  @override
+  void didUpdateWidget(covariant UsrInfoTabBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.selectedIndex != oldWidget.selectedIndex) {
+      selectedIndex = widget.selectedIndex;
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _updateUnderline();
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: SizedBox(
+      child: Container(
+        margin: const EdgeInsets.only(top: 20),
         height: 28,
         child: Stack(
           children: [
             // 회색 하단 선
             Positioned.fill(
               child: Container(
-                margin: const EdgeInsets.only(top:26),
+                margin: const EdgeInsets.only(top: 26),
                 height: 2,
                 color: const Color(0xFFEEEEEE),
               ),
             ),
             _buildUnderline(),
             Padding(
-              padding: const EdgeInsets.only(left: 20),
+              padding: const EdgeInsets.only(left: 20, right: 20),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, // 탭 간의 간격을 위해 start로 변경
                 children: List.generate(tabs.length, (index) {
                   final isSelected = index == selectedIndex;
-                  return Padding(
-                    padding: const EdgeInsets.only(right: 20),
-                    child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          selectedIndex = index;
-                          widget.onTap(selectedIndex);
-                        });
-                        WidgetsBinding.instance.addPostFrameCallback((_){
-                          _updateUnderline();
-                        });
-                      },
-                      child: Container(
-                        // 2. GlobalKey를 Container 위젯에 부여 => Container(=Text)의 위치/렌더링 정보에 접근할 수 있음
-                        key: _tabKeys[index],
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = index;
+                        widget.onTap(selectedIndex);
+                      });
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        _updateUnderline();
+                      });
+                    },
+                    child: Container(
+                      width: fixedTabWidth, // 탭의 너비를 고정
+                      alignment: Alignment.center,
+                      child: SizedBox(
+                        height: 44,
                         child: Text(
                           tabs[index],
                           style: TextStyle(
                             color: isSelected ? Colors.black : const Color(0xFFAAAAAA),
                             fontSize: 16,
                             fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w700,
-                            height: 1.50,
+                            height: 0.09, // 텍스트 높이 문제 방지를 위해 적정값 유지
                           ),
                         ),
                       ),
@@ -99,21 +113,16 @@ class _UsrInfoTabBarState extends State<UsrInfoTabBar> {
   }
 
   void _updateUnderline() {
-    // 3. Container(=Text)에 부여된 GlobalKey 꺼냄
-    final key = _tabKeys[selectedIndex];
-    // 4. GlobalKey로 RenderBox를 가져옴 -> 이게 있어야 위치와 크기 알 수 있음
-    final renderBox = key.currentContext?.findRenderObject() as RenderBox?;
-    if (renderBox != null) {
-      // 해당 탭 텍스트가 스크린 전체에서 어디에 위치해 있는지를 구한다.
-      final position = renderBox.localToGlobal(Offset.zero, ancestor: context.findRenderObject());
-      final left = position.dx; // 패딩 보정
-      final width = renderBox.size.width;
-      // underline을 해당 탭이 위치한 위치로 이동시킴
-      setState(() {
-        underlineLeft = left;
-        underlineWidth = width;
-      });
-    }
+    // 탭의 위치를 계산하여 언더라인 위치를 업데이트
+    const leftPadding = 20.0;
+    final totalSpacing = selectedIndex * tabSpacing;
+    final totalWidth = selectedIndex * fixedTabWidth;
+    final newLeft = leftPadding + totalWidth + totalSpacing;
+    
+    setState(() {
+      underlineLeft = newLeft;
+      underlineWidth = fixedTabWidth;
+    });
   }
 
   Widget _buildUnderline() {
@@ -122,10 +131,7 @@ class _UsrInfoTabBarState extends State<UsrInfoTabBar> {
       height: 2,
       color: Colors.black,
     );
-    /*  Positioned 와 AnimatedPositioned 으로 분기하는 이유는
-        페이지 첫 진입시 캐시된 하위 페이지를 불러올때 애니메이션 효과로 underline이 
-        움직이면 부자연스러워 보여서다.
-    */
+    
     return _isFirstBuild
         ? Positioned(left: underlineLeft, top: 26, child: underline)
         : AnimatedPositioned(
