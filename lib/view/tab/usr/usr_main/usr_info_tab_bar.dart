382c18c09f7e0c61b5c39eec13f7b64f286a10ee
dkdkdkdk555 <dkdkdkdk555@naver.com>
2025-08-26 23:12:38 +0900

    탭바에 반응형 적용

--- lib/view/tab/usr/usr_main/usr_info_tab_bar.dart
+++ lib/view/tab/usr/usr_main/usr_info_tab_bar.dart
@@ -18,26 +18,15 @@ class _UsrInfoTabBarState extends State<UsrInfoTabBar> {
   final List<String> tabs = ['뱃지', '신체 정보', '내가 쓴 글'];
   
   late int selectedIndex;
-
-  double underlineLeft = 0;
-  double underlineWidth = 0;
-
+  
+  static const double horizontalPadding = 20.0;
+  
   bool _isFirstBuild = true;
-
-  // 탭의 고정 너비와 간격을 정의합니다.
-  final double fixedTabWidth = 111.6; // 탭의 너비를 동일하게 맞추기 위한 임의의 값
 
   @override
   void initState() {
     super.initState();
     selectedIndex = widget.selectedIndex;
-    
-    // GlobalKey는 더 이상 텍스트 크기를 측정하는 용도가 아니므로 제거
-    // 대신, WidgetsBinding.instance.addPostFrameCallback 내에서 바로 위치를 계산합니다.
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _updateUnderline();
-      _isFirstBuild = false;
-    });
   }
   
   @override
@@ -45,99 +34,88 @@ class _UsrInfoTabBarState extends State<UsrInfoTabBar> {
     super.didUpdateWidget(oldWidget);
     if (widget.selectedIndex != oldWidget.selectedIndex) {
       selectedIndex = widget.selectedIndex;
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        _updateUnderline();
-      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Container(
-        margin: const EdgeInsets.only(top: 20),
-        height: 44,
-        child: Stack(
-          children: [
-            // 회색 하단 선
-            Positioned.fill(
-              child: Container(
-                margin: const EdgeInsets.only(top: 42),
-                height: 2,
-                color: const Color(0xFFEEEEEE),
-              ),
-            ),
-            _buildUnderline(),
-            Container(
-              padding: const EdgeInsets.only(left: 20, right: 20,),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: List.generate(tabs.length, (index) {
-                  final isSelected = index == selectedIndex;
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedIndex = index;
-                        widget.onTap(selectedIndex);
-                      });
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        _updateUnderline();
-                      });
-                    },
-                    child: Container(
-                      width: fixedTabWidth, // 탭의 너비를 고정
-                      alignment: Alignment.center,
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          tabs[index],
-                          style: TextStyle(
-                            color: isSelected ? Colors.black : const Color(0xFFAAAAAA),
-                            fontSize: 14.5,
-                            fontFamily: 'Pretendard',
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final double containerWidth = constraints.maxWidth;
+        final double usableWidth = containerWidth - (horizontalPadding * 2);
+        final double dynamicTabWidth = usableWidth / tabs.length;
+        
+        final double underlineLeft = horizontalPadding + (selectedIndex * dynamicTabWidth);
+        final double underlineWidth = dynamicTabWidth;
+
+        // Animate only after the first build
+        final Duration duration = _isFirstBuild ? Duration.zero : const Duration(milliseconds: 250);
+        
+        // This flag is updated here to ensure the animation is disabled only for the very first frame.
+        _isFirstBuild = false;
+
+        return SizedBox(
+          width: double.infinity,
+          child: Container(
+            margin: const EdgeInsets.only(top: 20),
+            height: 44,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 42),
+                    height: 2,
+                    color: const Color(0xFFEEEEEE),
+                  ),
+                ),
+                AnimatedPositioned(
+                  duration: duration,
+                  curve: Curves.easeInOut,
+                  left: underlineLeft,
+                  top: 42,
+                  child: Container(
+                    width: underlineWidth,
+                    height: 2,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: horizontalPadding),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: List.generate(tabs.length, (index) {
+                      final isSelected = index == selectedIndex;
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedIndex = index;
+                            widget.onTap(selectedIndex);
+                          });
+                        },
+                        child: Container(
+                          width: dynamicTabWidth,
+                          alignment: Alignment.center,
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              tabs[index],
+                              style: TextStyle(
+                                color: isSelected ? Colors.black : const Color(0xFFAAAAAA),
+                                fontSize: 14.5,
+                                fontFamily: 'Pretendard',
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                  );
-                }),
-              ),
-            )
-          ],
-        ),
-      ),
+                      );
+                    }),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
-  }
-
-  void _updateUnderline() {
-    // 탭의 위치를 계산하여 언더라인 위치를 업데이트
-    const leftPadding = 20.0;
-    final totalSpacing = selectedIndex;
-    final totalWidth = selectedIndex * fixedTabWidth;
-    final newLeft = leftPadding + totalWidth + totalSpacing;
-    
-    setState(() {
-      underlineLeft = newLeft;
-      underlineWidth = fixedTabWidth;
-    });
-  }
-
-  Widget _buildUnderline() {
-    final underline = Container(
-      width: underlineWidth,
-      height: 2,
-      color: Colors.black,
-    );
-    
-    return _isFirstBuild
-        ? Positioned(left: underlineLeft, top: 42, child: underline)
-        : AnimatedPositioned(
-            duration: const Duration(milliseconds: 250),
-            curve: Curves.easeInOut,
-            left: underlineLeft,
-            top: 42,
-            child: underline,
-          );
   }
 }
