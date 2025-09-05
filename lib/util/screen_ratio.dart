16187dc8f5021e1b12f25894a5f51b0f4c28829c
dkdkdkdk555 <dkdkdkdk555@naver.com>
2025-09-05 16:04:06 +0900

    스크린비율 계산 공통함수

---
+++ lib/util/screen_ratio.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class ScreenRatio {
+  final BuildContext context;
+  late final double widthRatio;
+  late final double heightRatio;
+
+  static const double baseWidth = 375.0; // 기준 기기 가로 (iPhone X 등)
+  static const double baseHeight = 812.0; // 기준 기기 세로
+
+  ScreenRatio(this.context) {
+    final screenSize = MediaQuery.of(context).size;
+    widthRatio = screenSize.width / baseWidth;
+    heightRatio = screenSize.height / baseHeight;
+  }
+}
