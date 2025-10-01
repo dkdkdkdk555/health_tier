111b040c65592a35e53b8e0b3839a88debf67422
dkdkdkdk555 <dkdkdkdk555@naver.com>
2025-10-01 22:06:21 +0900

    AppDelegate.swift 수정중

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -30,14 +30,7 @@ import flutter_local_notifications
       return true
     }
 
-    // 다른 플러그인이나 앱에서 URL 처리가 필요한 경우, 여기에 추가 로직을 넣을 수 있습니다.
-    // 예를 들어, 다른 소셜 로그인(카카오, 구글 등)이나 딥링크 처리가 있다면 이 부분에 추가합니다.
-    // 이 경우, `super.application` 호출을 통해 Flutter 플러그인들이 URL을 처리할 수 있도록 합니다.
-    // 현재는 Naver가 처리하지 않으면 바로 false를 반환하도록 되어 있으나,
-    // 다른 Flutter 플러그인들이 URL을 처리해야 한다면 아래 라인을 사용하세요.
-    // return super.application(app, open: url, options: options)
-
-    // Naver가 처리하지 않았고, 다른 플러그인 처리도 필요 없다면 false 반환
-    return false
+  // 다른 플러그인 처리
+    return super.application(app, open: url, options: options)
   }
 }
